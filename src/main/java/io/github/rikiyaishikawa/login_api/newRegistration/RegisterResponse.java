ff8a7cac50a1b94ce922c8a3e8516cec41ca21eb
Rikiya Ishikawa <flower.night.flower@gmail.com>
2025-11-17 17:35:28 +0900

    RegisterServiceとRegisterResponseを追加

---
+++ src/main/java/io/github/rikiyaishikawa/login_api/newRegistration/RegisterResponse.java
@@ -0,0 +1,47 @@
+package io.github.rikiyaishikawa.login_api.newRegistration;
+
+public class RegisterResponse {
+    private Long id;                 // DBの主キー
+    private String email;         // ログインID（メールでもOK）
+    private String password;         // ハッシュ済みパスワード
+    private String displayName;      // 表示名（任意）
+
+    public RegisterResponse(Long id, String email, String password, String displayName) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.displayName = displayName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+}
