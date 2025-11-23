eedca6f940dc0620e97535ec43546ced2b53c180
Rikiya Ishikawa <flower.night.flower@gmail.com>
2025-11-23 22:20:51 +0900

    UserMapperを追加

---
+++ src/main/java/io/github/rikiyaishikawa/login_api/UserMapper.java
@@ -0,0 +1,19 @@
+package io.github.rikiyaishikawa.login_api;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface UserMapper {
+
+    @Insert("INSERT INTO users (name, email) VALUES (#{name}, #{email})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insert(User user);
+
+}
