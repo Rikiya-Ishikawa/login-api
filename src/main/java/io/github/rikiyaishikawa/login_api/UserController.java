870b9ed5181351b3c208e7a3c0488dfeee50b904
Rikiya Ishikawa <flower.night.flower@gmail.com>
2025-11-23 22:03:22 +0900

    UserController.javaの追加

---
+++ src/main/java/io/github/rikiyaishikawa/login_api/UserController.java
@@ -0,0 +1,26 @@
+package io.github.rikiyaishikawa.login_api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> insert(@RequestBody UserRequest userRequest, UriComponentsBuilder uriBuilder) {
+        User user = userService.insert(userRequest.getName(), userRequest.getEmail());
+        URI location = uriBuilder.path("/users/{id}").buildAndExpand(user.getId()).toUri();
+        UserResponse body = new UserResponse("user created");
+        return ResponseEntity.created(location).body(body);
+    }
+
+}
