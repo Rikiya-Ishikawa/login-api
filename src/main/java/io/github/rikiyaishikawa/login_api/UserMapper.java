ef1c671de7dfc14418e24dbb63ced298b05c0fde
Rikiya Ishikawa <flower.night.flower@gmail.com>
2025-11-24 21:06:32 +0900

    UserMapperにfindByUsernameメソッドを追加

--- src/main/java/io/github/rikiyaishikawa/login_api/UserMapper.java
+++ src/main/java/io/github/rikiyaishikawa/login_api/UserMapper.java
@@ -3,9 +3,13 @@ package io.github.rikiyaishikawa.login_api;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
+
+    @Select("SELECT id, username, password, email FROM users WHERE username = #{username}")
+    User findByUsername(String username);
 
     @Insert("INSERT INTO users (username, password, email, role) VALUES (#{username}, #{password}, #{email), #{role}")
     @Options(useGeneratedKeys = true, keyProperty = "id")
