1c361fd6fd0bb62ed9e69c0478366c42cf69cb6f
Rikiya Ishikawa <flower.night.flower@gmail.com>
2025-11-24 15:17:08 +0900

    insertメソッドの引数を修正

--- src/main/java/io/github/rikiyaishikawa/login_api/UserMapper.java
+++ src/main/java/io/github/rikiyaishikawa/login_api/UserMapper.java
@@ -1,19 +1,14 @@
 package io.github.rikiyaishikawa.login_api;
 
-import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
-import java.util.List;
-import java.util.Optional;
 
 @Mapper
 public interface UserMapper {
 
     @Insert("INSERT INTO users (username, password, email, role) VALUES (#{username}, #{password}, #{email), #{role}")
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    User insert(UserRequest userRequest);
+    User insert(User user);
 
 }
